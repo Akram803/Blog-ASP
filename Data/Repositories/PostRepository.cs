9afce72c1506a70480abd78b539444498eab5531
Akram <aminakram379@gmail.com>
2021-07-17 18:33:44 +0200

    .

--- Data/Repositories/PostRepository.cs
+++ Data/Repositories/PostRepository.cs
@@ -56,6 +56,31 @@ namespace Blog.Data.Repositories
                             );
         }
 
+        public async Task<PagedList<Post>> GetFeed(PagingParametersVM pagingParameters, string[] blogersId)
+        {
+            var query = _context.Posts
+                                .AsNoTracking()
+                                .AsQueryable();
+
+            query.Where(p => 
+                    blogersId.Any( id => id == p.UserId ) 
+                );
+
+            int count = await query.CountAsync();
+            query = query
+                    .OrderByDescending(p => p.CreatedAt)
+                    .Skip((pagingParameters.PageNumber - 1) * pagingParameters.PageSize)
+                    .Take(pagingParameters.PageSize);
+
+            query = query.Include(p => p.User);
+            return new PagedList<Post>(
+                                await query.ToListAsync(),
+                                count,
+                                pagingParameters.PageNumber,
+                                pagingParameters.PageSize
+                            );
+        }
+
         public async Task<Post> GetByIdFull(int id)
         {
             var post = await _context.Posts
